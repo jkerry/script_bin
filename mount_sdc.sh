5b4099705c9e84cc87e06cee15eba65bcaa60e9a
John.Kerry <jk185160@ncr.com>
2017-06-23 16:01:21 -0400

    makign the dev variable based on mount inspection

--- mount_sdc.sh
+++ mount_sdc.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
+dev=""
+mount | grep "sda"
+if [ $? -eq 1 ]
+  then dev="sda"
+fi
+mount | grep "sdb"
+if [ $? -eq 1 ]
+  then dev="sdb"
+fi
+mount | grep "sdc"
+if [ $? -eq 1 ]
+  then dev="sdc"
+fi
+
+if [ "$dev" = "" ]
+  then exit 0
+fi
+
 echo "n
 p
 1
 
 
 w
-" | fdisk /dev/sdc
-mkfs.ext4 -L /data /dev/sdc1
+" | fdisk /dev/$dev
+mkfs.ext4 -L /data "/dev/${dev}1"
 mkdir /data
 echo "LABEL=/data /data      ext4    defaults        1 2" >> /etc/fstab
 mount -a
